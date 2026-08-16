045274354ea177a7dcba2fcccf9e95e531457eb4
Tanmay0045 <tanmay0000045@gmail.com>
2026-08-16 09:31:40 +0530

    ✅ Say "Hello, World!" With C++ [cpp20] — approach1

---
+++ solutions/cpp20/easy/cpp-hello-world/solution-approach1.cpp
@@ -0,0 +1,16 @@
+// ──────────────────────────────────────────────────
+// Link        https://www.hackerrank.com/challenges/cpp-hello-world/problem?isFullScreen=true
+// Problem     Say "Hello, World!" With C++
+// Difficulty  Easy
+// Subdomain   Introduction
+// Platform    HackerRank
+// Language    cpp20
+// Status      Accepted
+// Submitted   2026-08-16, 09:31 a.m.
+// ──────────────────────────────────────────────────
+
+#include<iostream>
+using namespace std;
+int main(){
+    cout<<"Hello, World!"<<endl;
+}
