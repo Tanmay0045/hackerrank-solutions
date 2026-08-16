1171b0a014059754240aab62b6624d39cebb45c9
Tanmay0045 <tanmay0000045@gmail.com>
2026-08-16 09:46:41 +0530

    ✅ Conditional Statements [cpp20] — approach1

---
+++ solutions/cpp20/easy/c-tutorial-conditional-if-else/solution-approach1.cpp
@@ -0,0 +1,64 @@
+// ──────────────────────────────────────────────────
+// Link        https://www.hackerrank.com/challenges/c-tutorial-conditional-if-else/problem?isFullScreen=true
+// Problem     Conditional Statements
+// Difficulty  Easy
+// Subdomain   Introduction
+// Platform    HackerRank
+// Language    cpp20
+// Status      Accepted
+// Submitted   2026-08-16, 09:46 a.m.
+// ──────────────────────────────────────────────────
+
+#include<iostream>
+using namespace std;
+int main(){
+    int n;
+    
+    cin>>n;
+    
+    if(n==1){
+        cout<<"one"<<endl;
+    }
+    
+    if(n==2){
+        cout<<"two"<<endl;
+    }
+    
+    if(n==3){
+        cout<<"three"<<endl;
+    }
+    
+    if(n==4){
+        cout<<"four"<<endl;
+    }
+    
+    if(n==5){
+        cout<<"five"<<endl;
+    }
+    
+    if(n==6){
+        cout<<"six"<<endl;
+    }
+    
+    if(n==7){
+        cout<<"seven"<<endl;
+    }
+    
+    if(n==8){
+        cout<<"eight"<<endl;
+    }
+    
+    if(n==9){
+        cout<<"nine"<<endl;
+    }
+    
+    else if(n>9){
+        cout<<"Greater than 9"<<endl;
+    }
+    
+    else{
+        cout<<""<<endl;
+    }
+    
+    
+}
