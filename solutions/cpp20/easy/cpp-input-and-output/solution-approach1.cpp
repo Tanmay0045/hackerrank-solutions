f3263e336a72e19d75e3abe6b54cf7eb0e41cde4
Tanmay0045 <tanmay0000045@gmail.com>
2026-08-16 09:34:54 +0530

    ✅ Input and Output [cpp20] — approach1

---
+++ solutions/cpp20/easy/cpp-input-and-output/solution-approach1.cpp
@@ -0,0 +1,19 @@
+// ──────────────────────────────────────────────────
+// Link        https://www.hackerrank.com/challenges/cpp-input-and-output/problem?isFullScreen=true
+// Problem     Input and Output
+// Difficulty  Easy
+// Subdomain   Introduction
+// Platform    HackerRank
+// Language    cpp20
+// Status      Accepted
+// Submitted   2026-08-16, 09:34 a.m.
+// ──────────────────────────────────────────────────
+
+#include<iostream>
+using namespace std;
+int main(){
+    int a,b,c;
+    cin >> a >> b >> c;
+    cout<<a+b+c<<endl;
+    return 0;
+}
