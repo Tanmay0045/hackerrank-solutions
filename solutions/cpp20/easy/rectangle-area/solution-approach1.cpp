82dc6f1943ec17dbb5e0259cd47357d1925f096b
Tanmay0045 <tanmay0000045@gmail.com>
2026-08-20 23:23:57 +0530

    ✅ Rectangle Area [cpp20] — approach1

---
+++ solutions/cpp20/easy/rectangle-area/solution-approach1.cpp
@@ -0,0 +1,21 @@
+// ──────────────────────────────────────────────────
+// Link        https://www.hackerrank.com/challenges/rectangle-area/problem?isFullScreen=true
+// Problem     Rectangle Area
+// Difficulty  Easy
+// Subdomain   Inheritance
+// Platform    HackerRank
+// Language    cpp20
+// Status      Accepted
+// Submitted   2026-08-20, 11:23 p.m.
+// ──────────────────────────────────────────────────
+
+#include<iostream>
+using namespace std;
+int main(){
+    int width,height;
+    cin>>width;
+    cin>>height;
+    cout<<width<<" "<<height<<endl;
+    cout<<width*height<<endl;
+    return 0;
+}
